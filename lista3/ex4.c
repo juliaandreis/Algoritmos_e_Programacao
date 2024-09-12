9a3ce80b2319a70da861fe0423556e87562648a0
juliaandreis <juliarenata19@gmail.com>
2024-09-11 21:18:38 -0300

    exercicios

---
+++ lista3/ex4.c
@@ -0,0 +1,21 @@
+/*Crie um programa que verifica se um número inteiro informado pelo usuário é divisível por 3.*/
+
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    printf("Digite um número: ");
+    scanf("%d",&a);
+
+    if (a % 3 == 0)
+    {
+        printf("%d é divisível por 3.", a);
+    }
+    else
+    {
+        printf("%d não é divisível por 3.", a);
+    }
+    
+    return 0;
+}
---
+++ lista3/ex6.c
@@ -0,0 +1,22 @@
+/*Brincadeira do PAR ou ÍMPAR. Pergunte para o usuário se ele aposta em PAR ou ÍMPAR. Depois
+disso, peça para ele digitar um número de 0 a 5 (como se fosse mostrar os dedos da mão. Sorteie
+um número de 0 a 5 e some com o que o usuário digitou. Se o usuário escolheu PAR e o valor da
+soma for par OU se ele escolheu ÍMPAR e o valor da soma é ímpar, diga que ele venceu. Senão, diga
+que o programa venceu*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char paridade;
+    int a,n;
+    printf("Você aposta par ou ímpar? ");
+    scanf("%s",&paridade);
+    printf("Digite um número: ");
+    scanf("%d",&a);
+
+    n = 1 + rand() % (5)
+
+    return 0;
+}

b2d53580965cacf359dd788c76a9283980c1620c
juliaandreis <juliarenata19@gmail.com>
2024-09-18 20:07:18 -0300

    aula

---
+++ aulas/ExemploContadorWhile.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int cont = 1; //sempre iniciar o contador, pois a variável começã com "sujeira" na memória
+
+    while (cont <= 5)
+    {
+        printf("olá mundo! contador: %d\n", cont);
+        cont++; //incrementa o contador (conta = cont + 1). Para diminuir, "cont--"
+    }
+
+    return 0;
+}
--- lista3/ex6.c
+++ lista3/ex6.c
@@ -6,17 +6,48 @@ que o programa venceu*/
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main()
 {
-    char paridade[5];
-    int a,n;
-    printf("Você aposta par ou ímpar? ");
+    srand (time(0));
+    char paridade;
+    int a,n,resultado;
+    printf("Você aposta par ou ímpar? Digite 'p' para par e 'i' para ímpar: ");
     scanf("%s",&paridade);
-    printf("Digite um número: ");
+    printf("Digite um número de 0 a 5: ");
     scanf("%d",&a);
 
-    n = 1 + rand() % (5)
+    if ((n < 6) && (paridade == 'p' || 'i'))
+    {
+        n = 0 + rand() % (6);
+
+        resultado = n + a;
+
+        printf("Número do oponente = %d.\n", n);
+        printf("Resultado = %d.\n", resultado);
+
+        if (((resultado) % 2 == 0) && paridade == 'p')
+        {
+            printf("Você ganhou!");
+        }
+        else if ((((resultado) % 2 != 0) && paridade == 'i'))
+        {
+            printf("Você ganhou!");
+        }
+        else if (((resultado) % 2 == 0) && paridade == 'i')
+        {
+            printf("Você perdeu!");
+        }
+        else
+        {
+            printf("Você perdeu!");
+        }
+    }
+    else
+    {
+        printf("Você não digitou os dados corretamente. Tente novamente.");
+    }
 
     return 0;
 }

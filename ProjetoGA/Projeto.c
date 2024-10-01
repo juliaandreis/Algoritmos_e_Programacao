79f5da06d98453af71e98668d1f8e1118d0c60c3
juliaandreis <juliarenata19@gmail.com>
2024-10-01 01:14:51 -0300

    projeto GA

--- ProjetoGA/Projeto.c
+++ ProjetoGA/Projeto.c
@@ -1,3 +1,5 @@
+/*nao ta funcionando o sorteio das condiçoes climaticas ou nao ta entrando na variavel clima*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -5,10 +7,10 @@
 int main()
 {
 	srand(time(0));
-	int menu,taxa,ciclo;
+	int menu,taxa,ciclo,populacaoFinal,populacaoInicial;
 	int cont = 1;
 	int sorteio = 0;
-	float populacaoFinal,populacaoInicial;
+	char clima[25]; // COMO DECLARAR A VARIAVEL VAZIA?
 
 	printf("Bem vindo ao programa de simulacao de crescimento bacteriano!\n");
 
@@ -32,6 +34,7 @@ int main()
 			scanf("%d", &ciclo);
 			
 			menu = 0;
+			cont = 0;
 		
 			while (cont <= ciclo)
 			{
@@ -40,25 +43,35 @@ int main()
 				if (sorteio <= 20) // alta temperatura
 				{
 					populacaoFinal = (float)populacaoFinal * 0.7;
+					clima = "alta temperatura"; // PQ AQUI E EMBAIXO TA DANDO ERRO????????????????????
 				}
 				else if (sorteio <= 35) // falta de nutrientes
 				{
 					populacaoFinal = (float)populacaoFinal * 0.75;
+					clima = "falta de nutrientes";
 				}
 				else if (sorteio <= 45) // excesso de umidade
 				{
 					populacaoFinal = (float)populacaoFinal * 0.8;
+					clima = "excesso de umidade";
 				}
 				else if (sorteio <= 50) // radiacao ultravioleta
 				{
 					populacaoFinal = (float)populacaoFinal * 0.5;
+					clima = "radiacao ultravioleta";
 				}
 				else if (sorteio <= 60) // condicao favoravel
 				{
 					populacaoFinal = (float)populacaoFinal * 1.2;
+					clima = "condicao favoravel";
 				}
 				
-				printf("Ciclo %d = %f\n",cont,populacaoFinal);
+				printf("Ciclo %d = %d",cont,populacaoFinal);
+				if (clima != "\0")
+				{
+					printf("condição adversa: %s", clima);
+				}
+				
 				populacaoInicial = populacaoFinal;
 				cont++;
 			}

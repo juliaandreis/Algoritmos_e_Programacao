3233bdf96a57aeeedf5e2368fb83e0e8f951d76b
juliaandreis <juliarenata19@gmail.com>
2024-10-02 13:42:03 -0300

    projeto GA

--- ProjetoGA/Projeto.c
+++ ProjetoGA/Projeto.c
@@ -7,23 +7,24 @@
 int main()
 {
 	srand(time(0));
-	int menu,taxa,ciclo,populacaoFinal,populacaoInicial,clima;
+	int menu,taxa,ciclo,populacaoFinal,populacaoInicial;
 	int cont = 1;
 	int sorteio = 0;
 
 	printf("Bem vindo ao programa de simulacao de crescimento bacteriano!\n");
+	printf("\n");
 
 	do
 	{
 		printf("1 - Nova simulacao\n");
 		printf("2 - Sair do programa\n");
-		printf("Escolha uma opção: ");
+		printf("Escolha uma opcao: ");
 		scanf("%d", &menu);
 		printf("\n");
 
 		if (menu != 1 && menu != 2)
 		{
-			printf("Opção invalida. Tente novamente.\n");
+			printf("Opcao invalida. Tente novamente.\n");
 		}
 		else if (menu == 1)
 		{
@@ -35,81 +36,52 @@ int main()
 			scanf("%d", &ciclo);
 			
 			printf("\n");
-			
-			sorteio = 0;
-			menu = 0;
 			cont = 1;
-		
+
 			while (cont <= ciclo)
 			{
 				populacaoFinal = populacaoInicial + (populacaoInicial * taxa / 100);
 				sorteio = 1 + rand () % 100;
 				
-				if (sorteio <= 20) // alta temperatura
+				if (sorteio <= 20)
 				{
 					populacaoFinal = (float)populacaoFinal * 0.7;
-					clima = 1;
-				}
-				else if (sorteio <= 35) // falta de nutrientes
-				{
-					populacaoFinal = (float)populacaoFinal * 0.75;
-					clima = 2;
-				}
-				else if (sorteio <= 45) // excesso de umidade
-				{
-					populacaoFinal = (float)populacaoFinal * 0.8;
-					clima = 3;
-				}
-				else if (sorteio <= 50) // radiacao ultravioleta
-				{
-					populacaoFinal = (float)populacaoFinal * 0.5;
-					clima = 4;
-				}
-				else if (sorteio <= 60) // condicao favoravel
-				{
-					populacaoFinal = (float)populacaoFinal * 1.2;
-					clima = 5;
-				}
-				else
-				{
-					clima = 0;
-				}
-
-				//saída de dados:
-				if (clima == 1)
-				{
 					printf("Ciclo %d = %d (condição adversa: alta temperatura)\n",cont,populacaoFinal);
 				}
-				else if (clima == 2)
+				else if (sorteio <= 35)
 				{
+					populacaoFinal = (float)populacaoFinal * 0.75;
 					printf("Ciclo %d = %d (condição adversa: falta de nutrientes)\n",cont,populacaoFinal);
 				}
-				else if (clima == 3)
+				else if (sorteio <= 45)
 				{
+					populacaoFinal = (float)populacaoFinal * 0.8;
 					printf("Ciclo %d = %d (condição adversa: excesso de umidade)\n",cont,populacaoFinal);
+
 				}
-				else if (clima == 4)
+				else if (sorteio <= 50)
 				{
+					populacaoFinal = (float)populacaoFinal * 0.5;
 					printf("Ciclo %d = %d (condição adversa: radiacao ultravioleta)\n",cont,populacaoFinal);
 				}
-				else if (clima == 5)
+				else if (sorteio <= 60)
 				{
+					populacaoFinal = (float)populacaoFinal * 1.2;
 					printf("Ciclo %d = %d (condicao favoravel)\n",cont,populacaoFinal);
 				}
 				else
 				{
-					printf("Ciclo %d = %d\n",cont,populacaoFinal);//população final vai ficar como %d ou %f?
+					printf("Ciclo %d = %d\n",cont,populacaoFinal);
 				}
 
-				//printf("sorteio: %d\n", sorteio);
+				sorteio = 0;
 				populacaoInicial = populacaoFinal;
 				cont++;
 			}
-			
+			printf("\n");
+			printf("Simulacao concluida. Populacao final: %d\n", populacaoFinal);
 		}
-	printf("\n");
-	printf("Simulacao concluida. Populacao final: %d\n", populacaoFinal);
-	printf("\n");
+		printf("\n");
 
 	} while (menu != 2);
 	

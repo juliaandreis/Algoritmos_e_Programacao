1a01cdd8bf6489459d93733fe9ba85aadb79847a
juliaandreis <juliarenata19@gmail.com>
2024-11-27 12:22:42 -0300

    simulado

--- Simulado GB/Ex4.c
+++ Simulado GB/Ex4.c
@@ -12,12 +12,47 @@ e) Cadastre 5 livros.
 f) Após o cadastro, exiba todos os livros cadastrados, usando a função exibirLivro*/
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <string.h>
+
+typedef struct {
+    char titulo[100], autor[100];
+    int ano, paginas;
+} Livro;
+
+Livro cadastrarLivro(char titulo[], char autor[], int ano, int paginas)
+{
+    Livro livro;
+    strcpy(livro.titulo, titulo);
+    strcpy(livro.autor, autor);
+    livro. ano = ano;
+    livro.paginas = paginas;
+
+    return livro;
+}
+
+void exibirLivro(Livro livro)
+{
+    printf("Título: %s\n", livro.titulo);
+    printf("Autor: %s\n", livro.autor);
+    printf("Ano de publicação: %d\n", livro.ano);
+    printf("Páginas: %d\n", livro.paginas);
+}
 
 int main()
 {
+    Livro biblioteca[5];
 
+    biblioteca[0] = cadastrarLivro("1984", "George Orwell", 1949, 328);
+    biblioteca[1] = cadastrarLivro("Dom Quixote", "Miguel de Cervantes", 1605, 863);
+    biblioteca[2] = cadastrarLivro("Orgulho e Preconceito", "Jane Austen", 1813, 279);
+    biblioteca[3] = cadastrarLivro("O Senhor dos Anéis", "J.R.R. Tolkien", 1954, 1178);
+    biblioteca[4] = cadastrarLivro("O Pequeno Príncipe", "Antoine de Saint-Exupéry", 1943, 96);
+
+    for(int i=0; i<5; i++)
+    {
+        exibirLivro(biblioteca[i]);
+        printf("\n");
+    }
 
     return 0;
 }
